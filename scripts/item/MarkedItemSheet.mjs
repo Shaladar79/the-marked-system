9819a70f66ff3ce425c3db125e96a90af3b5adab
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-11-18 10:50:23 -0600

    Update MarkedItemSheet.mjs

--- scripts/item/MarkedItemSheet.mjs
+++ scripts/item/MarkedItemSheet.mjs
@@ -1,1 +1,13 @@
+// scripts/item/MarkedItemSheet.mjs
+
+export class MarkedItemSheet extends ItemSheet {
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["marked", "sheet", "item"],
+      template: "systems/marked-system/templates/items/item-sheet.hbs",
+      width: 600,
+      height: 400
+    });
+  }
+}
 
