0029a1b18a233ceedca6e5758cbd57e8203ddedc
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-11-21 09:08:08 -0600

    Update MarkedActorSheet.mjs

--- scripts/actor/MarkedActorSheet.mjs
+++ scripts/actor/MarkedActorSheet.mjs
@@ -4,16 +4,19 @@ import { MarkedConfig } from "../config.mjs";
 
 export class MarkedActorSheet extends ActorSheet {
   static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["marked", "sheet", "actor"],
-      template: "systems/the-marked-system/templates/actors/actor-sheet.hbs",
-      width: 900,
-      height: 700,
-      tabs: [
-        { navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "main" }
-      ]
-    });
-  }
+  return foundry.utils.mergeObject(super.defaultOptions, {
+    classes: ["marked", "sheet", "actor"],
+    template: "systems/the-marked-system/templates/actors/actor-sheet.hbs",
+    width: 900,
+    height: 700,
+    tabs: [
+      { navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "main" }
+    ],
+    submitOnChange: true,
+    submitOnClose: true
+  });
+}
+
 
   getData(options) {
     const data = super.getData(options);
